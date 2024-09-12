b6ea78769eb8ac998a02fdc9ebe534deb6735de9
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2024-09-11 21:26:22 -0500

    create: AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { getCurrentUser } from '@/lib/appwrite/api';
 import { IContextType, IUser } from '@/types';
 import { createContext, useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const INITIAL_USER = {
   id: '',
@@ -26,6 +27,8 @@ const AuthProvider = ({ children }: {children: React.ReactNode}) => {
   const [user, setUser] = useState<IUser>(INITIAL_USER);
   const [isLoading, setIsLoading] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const navigate = useNavigate();
 
   const checkAuthUser = async () => {
     try {
@@ -55,6 +58,15 @@ const AuthProvider = ({ children }: {children: React.ReactNode}) => {
     }
   };
 
+  useEffect(() => {
+    if(
+      localStorage.getItem('cookieFallback') === '[]' || 
+      localStorage.getItem('cookieFallback') === null
+    ) navigate('/sign-in');
+
+    checkAuthUser();
+  }, []);
+
   const value = {
     user,
     setUser,
@@ -66,9 +78,11 @@ const AuthProvider = ({ children }: {children: React.ReactNode}) => {
 
   return (
     <AuthContext.Provider value={value}>
-      
+      {children}
     </AuthContext.Provider>
   )
 }
 
-export default AuthContext
+export default AuthProvider
+
+export const useUserContext = () => useContext(AuthContext)
